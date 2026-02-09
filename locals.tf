83c63422df5b74458ec06af0f94b0f151f8f1df9
AgudaruStefan <97911661+AgudaruStefan@users.noreply.github.com>
2026-02-10 01:13:35 +0200

    feat: Add AWS resource workspaces with configuration variables (#1)

--- locals.tf
+++ locals.tf
@@ -19,5 +19,61 @@ locals {
       project_id     = module.project["fem-eci-project"].id
       vcs_repo_identifier = "${var.github_organization_name}/PoC-GitOps-Terraform-Github"
     }
+
+    "fem-eci-aws-network" = {
+      description         = "Automation for AWS network resources."
+      execution_mode      = "remote"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/PoC-GitOps-Terraform-aws-network"
+
+      variables = [
+        {
+          category = "terraform"
+          hcl      = true
+          key      = "azs"
+          value    = jsonencode(["eu-central-1", "eu-north-1"])
+        },
+        {
+          category = "terraform"
+          key      = "cidr"
+          value    = "10.0.0.0/16"
+        },
+        {
+          category = "terraform"
+          key      = "name"
+          value    = "fem-eci"
+        },
+      ]
+    }
+
+    "fem-eci-aws-cluster-prod" = {
+      description         = "Automation for AWS cluster resources."
+      execution_mode      = "remote"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/PoC-GitOps-Terraform-aws-cluster"
+
+      variables = [
+        {
+          category = "terraform"
+          key      = "domain"
+          value    = "morse.beer"
+        },
+        {
+          category = "terraform"
+          key      = "environment"
+          value    = "prod"
+        },
+        {
+          category = "terraform"
+          key      = "name"
+          value    = "fem-eci-beardedsam"
+        },
+        {
+          category = "terraform"
+          key      = "vpc_name"
+          value    = "fem-eci"
+        },
+      ]
+    }
   }
 }
