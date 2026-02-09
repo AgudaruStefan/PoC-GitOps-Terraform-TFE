4ce63d3d5b85a56b8b5653e98df16ec853b77246
AgudaruStefan <agudarustefan@gmail.com>
2026-02-09 22:49:49 +0200

    feat: Add data source for GitHub app installation and update workspace configuration

--- variables.tf
+++ variables.tf
@@ -2,3 +2,12 @@ variable "organization_name" {
   default = "BeardedNetwork"
   type    = string
 }
+
+variable "github_app_installation_id" {
+  default = 109077727
+}
+
+variable "github_organization_name" {
+  default = "AgudaruStefan"
+  type    = string
+} 
